02a6a1d5cc524ebf987d88819945059810d6e0d5
marjorienoda <marjorie_uimie@hotmail.com>
2026-02-07 14:06:56 +0900

    add ex05

---
+++ 2026-feb/c_piscine/c_01/ex05/ft_putstr.h
@@ -0,0 +1,5 @@
+#define FT_PUTSTR_H
+
+#include <unistd.h>
+
+void ft_putstr(char *str);
---
+++ 2026-feb/c_piscine/c_01/ex05/main.c
@@ -0,0 +1,11 @@
+#include "ft_putstr.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char words[] = "Hoje esta chovendo!";
+    char *ptr = words;
+
+    ft_putstr(ptr);
+
+}
