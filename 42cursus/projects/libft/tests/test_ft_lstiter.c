6ead604548b920776b86938856131243bf42679e
marjorienoda <marjorie_uimie@hotmail.com>
2026-05-07 10:33:20 +0900

    add ft_lstiter

---
+++ 42cursus/projects/libft/tests/test_ft_lstiter.c
@@ -0,0 +1,46 @@
+#include "tests.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void upper_char(void *content)
+{
+    char *s = (char *)content;
+    int i;
+    i = 0;
+    while(s[i])
+    {
+        s[i] = (char)ft_toupper((int)s[i]);
+        i++;
+    }
+}
+
+void	print_list(t_list *lst)
+{
+	int i = 0;
+
+	while (lst)
+	{
+		printf("Node %d: %s\n", i, (char *)lst->content);
+		lst = lst->next;
+		i++;
+	}
+}
+
+void test_ft_lstiter(void)
+{
+    printf("--- Testing ft_lstiter ---\n");
+    t_list *list = ft_lstnew(ft_strdup("hello"));
+    ft_lstadd_back(&list, ft_lstnew(ft_strdup("marjorie")));
+    ft_lstadd_back(&list, ft_lstnew(ft_strdup("42tokyo")));
+
+    printf("--- Before ---\n");
+    print_list(list);
+
+
+    ft_lstiter(list->next, upper_char);
+
+    printf("--- After ---\n");
+    print_list(list);
+
+    ft_lstclear(&list, free);
+}
