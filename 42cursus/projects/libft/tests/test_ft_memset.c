4dc0468daf57029b1f59578aead3f43342db0680
marjorienoda <marjorie_uimie@hotmail.com>
2026-05-07 21:51:05 +0900

    remove comments

--- 42cursus/projects/libft/tests/test_ft_memset.c
+++ 42cursus/projects/libft/tests/test_ft_memset.c
@@ -4,14 +4,12 @@
 
 void run_test(char *str_ft, char *str_orig, int c, size_t n)
 {
-    // Executa ambas as funções
     ft_memset(str_ft, c, n);
     memset(str_orig, c, n);
 
     printf("Test: Fill %zu bytes with '%c'\n", n, c);
     printf("Expected: [%s] | Actual: [%s] \n", str_orig, str_ft);
 
-    // Compara o conteúdo dos buffers após a operação
     if (memcmp(str_ft, str_orig, 15) == 0)
         printf("✅ SUCCESS\n");
     else
@@ -30,7 +28,6 @@ void test_ft_memset(void)
     char b4[15] = {0};
     run_test(b3, b4, 'Z', 5);
 
-    // Teste 3: n = 0 (Não deve alterar nada)
     char b5[15] = "42Tokyo";
     char b6[15] = "42Tokyo";
     run_test(b5, b6, 'X', 0);
