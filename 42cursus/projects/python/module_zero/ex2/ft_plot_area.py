01f2b03606fcdbda98e5384a640bd43daa91e8e7
marjorienoda <marjorie_uimie@hotmail.com>
2026-06-18 15:55:44 +0900

    add python module zero

---
+++ 42cursus/projects/python/module_zero/ex2/ft_plot_area.py
@@ -0,0 +1,7 @@
+def ft_plot_area():
+    length_input = input("Enter length: ")
+    width_input = input("Enter width: ")
+    length = int(length_input)
+    width = int(width_input)
+    area = length * width
+    print(f"Plot area: {area}")
