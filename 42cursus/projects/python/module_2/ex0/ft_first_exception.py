bb97eda987c2b5c26c6b9225cd972407000f56e0
marjorienoda <marjorie_uimie@hotmail.com>
2026-06-22 13:41:27 +0900

    add ex0 module 2

---
+++ 42cursus/projects/python/module_2/ex0/ft_first_exception.py
@@ -0,0 +1,20 @@
+def input_temperature(temp_str: str) -> int:
+    temp_int = int(temp_str)
+    return temp_int
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===\n")
+    print("Input data is '25'")
+    try:
+        temp = input_temperature("25")
+        print(f"Temperature is now {temp}°C\n")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+    print("Input data is 'abc'")
+    try:
+        temp = input_temperature("abc")
+        print(f"Temperature is now {temp}°C")
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+    print("All tests completed - program didn't crash!")
