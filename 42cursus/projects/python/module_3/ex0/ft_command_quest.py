80fbbe430e6f071279c89ffebf0f7faf1ae6f0e0
marjorienoda <marjorie_uimie@hotmail.com>
2026-06-22 17:45:50 +0900

    add ex0

---
+++ 42cursus/projects/python/module_3/ex0/ft_command_quest.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def main():
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    total = len(sys.argv)
+    arguments = sys.argv[1:]
+    counter = 1
+    if total == 1:
+        print("No arguments provided!")
+    else:
+
+        for arg in arguments:
+            print(f"Argument: {counter}: {arg}")
+            counter += 1
+    print(f"Total arguments: {total}")
