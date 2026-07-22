766caaf2ce3080b9d774fca5faa67d3a41cbfb2d
marjorienoda <marjorie_uimie@hotmail.com>
2026-07-22 14:10:29 +0900

    add ex0

--- 42cursus/projects/python/module_3/ex0/ft_command_quest.py
+++ 42cursus/projects/python/module_3/ex0/ft_command_quest.py
@@ -10,8 +10,12 @@ def main() -> None:
     if total == 1:
         print("No arguments provided!")
     else:
-
+        print(f"Arguments received: {total - 1}")
         for arg in arguments:
-            print(f"Argument: {counter}: {arg}")
+            print(f"Argument {counter}: {arg}")
             counter += 1
     print(f"Total arguments: {total}")
+
+
+if __name__ == "__main__":
+    main()
